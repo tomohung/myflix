892f8aa1f2bf2b1aff2cd48f5176c74449eda621
Tomo Hung <tomohung@gmail.com>
2015-03-04 22:31:04 +0800

    finish search function and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-cat_comedy = Category.create(title: 'TV Comedies')
-cat_drama = Category.create(title: 'TV Dramas')
-cat_reality = Category.create(title: 'Reality TV')
+cat_comedy = Category.create(title: 'TV Comedies', description: 'tv comedies')
+cat_drama = Category.create(title: 'TV Dramas', description: 'tv dramas')
+cat_reality = Category.create(title: 'Reality TV', description: 'reality tv')
 
 v = cat_comedy.videos.build(title: 'South Park', description: 'funny cartoon', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg')
 v.save
@@ -16,5 +16,5 @@ v.save
 v = cat_drama.videos.build(title: 'Monk', description: 'Monk is... monk.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
 v.save
 
-v = cat_reality.videos.build(title: 'Reality Monk', description: 'Monk is still a monk.', small_cover_url: 'tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
+v = cat_reality.videos.build(title: 'Reality Monk', description: 'Monk is still a monk.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
 v.save
