a7bffb8436db4a98d79676f21d9626622bc4ca1b
Tomo Hung <tomohung@gmail.com>
2015-03-05 09:30:44 +0800

    use recent_videos for index.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,17 @@ cat_comedy = Category.create(title: 'TV Comedies', description: 'tv comedies')
 cat_drama = Category.create(title: 'TV Dramas', description: 'tv dramas')
 cat_reality = Category.create(title: 'Reality TV', description: 'reality tv')
 
-v = cat_comedy.videos.build(title: 'South Park', description: 'funny cartoon', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg')
-v.save
+10.times do |index|
+  v = cat_comedy.videos.build(title: "South Park - #{index}", description: 'funny cartoon', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/monk_large.jpg')
+  v.save
+end
 
-v = cat_drama.videos.build(title: 'Monk', description: 'Monk is... monk.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
-v.save
+10.times do |index|
+  v = cat_drama.videos.build(title: "Monk - #{index}", description: 'Monk is... monk.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
+  v.save
+end
 
-v = cat_reality.videos.build(title: 'Reality Monk', description: 'Monk is still a monk.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
-v.save
+10.times do |index|
+  v = cat_reality.videos.build(title: "Reality - #{index}", description: 'Monk is still a monk.', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg')
+  v.save
+end
