9139ef1bfe1c0b9f7e8f2cfaf152b97f56cef550
Tomo Hung <tomohung@gmail.com>
2015-03-07 20:59:23 +0800

    finish sessions_controller_spec and users_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -17,41 +17,74 @@ describe SessionsController do
   end
 
   describe 'POST create' do
-    it 'redirects to home_path if authenticated' do
-      user = Fabricate(:user)
-      post :create, email: user.email, password: user.password
-      expect(response).to redirect_to home_path
+    context 'with authenticated' do
+      before do 
+        user = Fabricate(:user)
+        post :create, email: user.email, password: user.password
+      end
+
+      it 'redirects to home_path' do
+        expect(response).to redirect_to home_path
+      end
+
+      it 'sets session[:user_id]' do
+        expect(session[:user_id]).to eq(User.first.id)
+      end
+
+      it 'sets notice' do
+        expect(flash[:notice]).not_to be_blank
+      end
     end
 
-    it 'redirects to sign_in_path if unauthenticated' do
-      post :create
-      expect(response).to redirect_to sign_in_path
+    context 'with unauthenticated' do
+      before do
+        user = Fabricate(:user)
+        post :create, email: user.email, password: user.password + 'lkjfd'
+      end
+
+      it 'redirects to sign_in_path' do
+        expect(response).to redirect_to sign_in_path
+      end
+
+      it 'does not set session[:user_id]' do
+        expect(session[:user_id]).to be_nil
+      end
+
+      it 'sets error message' do
+        expect(flash[:error]).not_to be_blank  
+      end
     end
-  end
+ end
 
   describe 'GET destroy' do
-    it 'sets session[:user_id] = nil if authenticated' do
-      user = Fabricate(:user)
-      session[:user_id] = user.id
-      get :destroy
-      expect(session[:user_id]).to be_nil
+    context 'with authenticated' do
+      before do
+        session[:user_id] = Fabricate(:user).id
+        get :destroy
+      end
+
+      it 'sets session[:user_id] = nil if authenticated' do
+        expect(session[:user_id]).to be_nil
+      end
+  
+      it 'redirects_to root_path if authenticated' do
+        expect(response).to redirect_to root_path
+      end
     end
 
-    it 'sets session[:user_id] to nil if unauthenticated' do
-      get :destroy
-      expect(session[:user_id]).to be_nil
-    end
+    context 'with unauthenticated' do
+      before { get :destroy }      
+      it 'sets session[:user_id] to nil if unauthenticated' do
+        expect(session[:user_id]).to be_nil
+      end
+  
+      it 'redirects_to root_path if unauthenticated' do
+        expect(response).to redirect_to root_path
+      end
 
-    it 'redirects_to root_path if authenticated' do
-      user = Fabricate(:user)
-      session[:user_id] = user.id
-      get :destroy
-      expect(response).to redirect_to root_path
-    end
-
-    it 'redirects_to root_path if unauthenticated' do
-      get :destroy
-      expect(response).to redirect_to root_path
+      it 'set notice' do
+        expect(flash[:notice]).not_to be_blank
+      end
     end
   end
 end
