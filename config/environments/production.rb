4f7d40657c50debe9a80b7036046c67377eebe25
Tomo Hung <tomohung@gmail.com>
2015-04-07 11:45:39 +0800

    fix mailgun setting again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,12 +18,13 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => "tomo-myflix.herokuapp.com" }
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'tomo-myflix.heroku.com',
+    :domain         => 'tomo-myflix.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
