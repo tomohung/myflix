8281fddd5a17cb67feb7b7d0d8659baadf7fe910
Tomo Hung <tomohung@gmail.com>
2015-04-10 11:16:35 +0800

    complete POST create for new admin video

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -8,15 +8,24 @@ class Admin::VideosController < ApplicationController
   end
 
   def create
-
-  end
-
-  private
-  def require_admin
-    if !current_user.admin?
-      flash[:danger] = 'You do not have access right.'
-      redirect_to home_path
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = "You have added a new video '#{@video.title}'."
+      redirect_to new_admin_video_path
+    else
+      render :new
     end
   end
 
+  private
+    def require_admin
+      if !current_user.admin?
+        flash[:danger] = 'You do not have access right.'
+        redirect_to home_path
+      end
+    end
+
+    def video_params
+      params.require(:video).permit(:title, :description, :category_id)
+    end
 end
--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -5,24 +5,78 @@ describe Admin::VideosController do
     it_behaves_like 'require_sign_in' do
       let(:action) { get :new }
     end
+
+    it_behaves_like 'require admin' do
+      let(:action) { get :new }
+    end
+
+    it 'sets the @video to a new video' do
+      set_current_admin
+      get :new
+      expect(assigns(:video)).to be_a_new Video
+    end
+  
+    it 'sets the flash error message for regular user' do
+      set_current_user
+      get :new
+      expect(flash[:danger]).to be_present
+    end
   end
 
-  it 'sets the @video to a new video' do
-    set_current_admin
-    get :new
-    expect(assigns(:video)).to be_a_new Video
-  end
-  
-  it 'redirects the regular user to home path' do
-    set_current_user
-    get :new
-    expect(response).to redirect_to home_path
-  end
-  
-  it 'sets the flash error message for regular user' do
-    set_current_user
-    get :new
-    expect(flash[:danger]).to be_present
+  describe 'POST create' do
+    it_behaves_like 'require_sign_in' do
+      let(:action) { post :create }
+    end
+
+    it_behaves_like 'require admin' do
+      let(:action) { post :create }
+    end
+
+    context 'with valid inputs' do
+      it 'creates a video' do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: {title: 'video', description: 'fun movie', category_id: category.id }
+        expect(category.videos.count).to eq(1)
+      end
+
+      it 'redirects to add new video page' do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: {title: 'video', description: 'fun movie', category_id: category.id }
+        expect(response).to redirect_to new_admin_video_path
+      end
+
+      it 'sets the flash success message' do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: {title: 'video', description: 'fun movie', category_id: category.id }
+        expect(flash[:success]).to be_present
+      end
+    end
+
+    context 'with invalid inputs' do
+      it 'does not create a video' do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { description: 'fun movie', category_id: category.id }
+        expect(category.videos.count).to eq(0)
+      end
+
+      it 'render the :new template' do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { description: 'fun movie', category_id: category.id }
+        expect(response).to render_template :new        
+      end
+
+      it 'sets the @video variable' do
+        set_current_admin
+        category = Fabricate(:category)
+        post :create, video: { description: 'fun movie', category_id: category.id }
+        expect(assigns(:video)).to be_instance_of Video
+      end
+    end
   end
 
 end
