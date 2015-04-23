e7acca701b94366e241603a482d2692169c46a49
Tomo Hung <tomohung@gmail.com>
2015-04-23 21:18:21 +0800

    fix test for loggin in users controller

--- spec/services/user_signup_spec.rb
+++ spec/services/user_signup_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe UserSignup do
-  describe "%sign_up" do
+  describe "#sign_up" do
     context 'with valid personal info and card' do
       before do
         charge = double(:charge, success?: true)
